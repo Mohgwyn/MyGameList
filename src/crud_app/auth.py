1b9fc8ec10d6130a056132bbeac3b5cf3edae701
Mohgwyn <fjgarciacobo@gmail.com>
2024-02-14 11:33:02 +0100

    Added password functionality

--- src/crud_app/auth.py
+++ src/crud_app/auth.py
@@ -1,14 +1,15 @@
 import os
 
 from db import DataBase
+import json
 
 
-def login(user: str, db: DataBase):
-    response = db.user_exists(user=user)
+def login(user: str, password: str, db: DataBase):
+    response = db.user_exists(user=user, password=password)
 
     if response is not None:
         with open(".cookie", "w") as f:
-            f.write(str(response["user_id"]))
+            f.write(json.dumps(response))
         print("Logged in as user with ID", str(response["user_id"]))
     else:
         print(f"Invalid user: {user}, contact your admin to create an account")
@@ -27,8 +28,8 @@ def logout():
 def is_logged_in(db: DataBase):
     try:
         with open(".cookie", "r") as f:
-            user_id = f.read()
-            if db.user_exists(user_id=user_id):
+            doc = json.loads(f.read())
+            if db.user_exists(user_id=doc['user_id'], password=['password']):
                 return True
             else:
                 return False
